900942e8a456b88fca966e844ab95d38aeb47fc8
Francisco Souza <f@souza.cc>
2012-07-27 12:14:37 -0300

    concorrência em python: adicionando código do hands on

---
+++ concorrencia-em-python/code/elevator.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import time
+
+import csp.csp as csp
+
+
+@csp.process
+def elevator(chan):
+    while True:
+        print "Transportando %s" % chan.read()
+        time.sleep(2)
+
+
+@csp.process
+def people(chan, names):
+    for name in names:
+        chan.write(name)
+    chan.poison()
+
+if __name__ == "__main__":
+    names = ["Francisco", "Chico", "Maria", "José"]
+    chan = csp.Channel()
+    csp.Par(elevator(chan), elevator(chan), elevator(chan), people(chan, names)).start()
